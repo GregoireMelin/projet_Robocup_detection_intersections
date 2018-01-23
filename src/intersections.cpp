1db82fae4ec852a6e938d51f5c2b1e8802203893
Florian Brenot <deamond4rk@gmail.com>
2018-01-23 23:09:19 +0100

    Modifs code final

--- src/intersections.cpp
+++ src/intersections.cpp
@@ -4,37 +4,37 @@
 using namespace std;
 using namespace cv;
 
-cv::Point2i GetIntersection(const cv::Point2i &o1, const cv::Point2i &p1, const cv::Point2i &o2, const cv::Point2i &p2) {
-  cv::Point2i x = o2 - o1;
-  cv::Point2i d1 = p1 - o1;
-  cv::Point2i d2 = p2 - o2;
+Point2i GetIntersection(const Point2i &o1, const Point2i &p1, const Point2i &o2, const Point2i &p2) {
+  Point2i x = o2 - o1;
+  Point2i d1 = p1 - o1;
+  Point2i d2 = p2 - o2;
 
   // Calculate cross product
   double cross = d1.x * d2.y - d1.y * d2.x;
 
   if (abs(cross) < 1e-8) {
-      return cv::Point2i();
+      return Point2i();
   }
 
   // Calculate angle between lines
   double angle_diff = atan2(p1.y - o1.y, p1.x - o1.x) - atan2(p2.y - o2.y, p2.x - o2.x);
 
   if (fabs(angle_diff) < 15.0 * CV_PI / 180.0) {
-      return cv::Point2i();
+      return Point2i();
   }
 
   // Calculate intersection point
   double t1 = (x.x * d2.y - x.y * d2.x) / cross;
-  cv::Point2i r(o1 + d1 * t1);
+  Point2i r(o1 + d1 * t1);
 
   if (r.x > 0 && r.x < 800 && r.y > 0 && r.y < 448) {
     return r;
   } else {
-    return cv::Point2i();
+    return Point2i();
   }
 }
 
-int GetIntersectType(const cv::Point2i &o1, const cv::Point2i &p1, const cv::Point2i &o2, const cv::Point2i &p2, const cv::Point2i &i) {
+int GetIntersectType(const Point2i &o1, const Point2i &p1, const Point2i &o2, const Point2i &p2, const Point2i &i) {
   double DO1I = norm(o1 - i);
   double DP1I = norm(p1 - i);
   double DO2I = norm(o2 - i);
@@ -44,7 +44,7 @@ int GetIntersectType(const cv::Point2i &o1, const cv::Point2i &p1, const cv::Poi
   double DP2O2 = norm(p2 - o2);
 
   int c = 0;
-  double max_dist = 10.0;
+  double max_dist = 50.0;
 
   if (DO1I < max_dist) c++; // Si O1 == I
   if (DP1I < max_dist) c++; // Si P1 == I
@@ -61,7 +61,7 @@ int GetIntersectType(const cv::Point2i &o1, const cv::Point2i &p1, const cv::Poi
   return c;
 }
 
-Mat filter(Mat src) 
+Mat filter(Mat src)
 {
   Mat picture_HSV;
   cvtColor(src, picture_HSV, COLOR_BGR2HSV);
@@ -84,78 +84,76 @@ Mat filter(Mat src)
   inRange(picture_HSV, minHSV, maxHSV, maskHSV);
   bitwise_and(picture_HSV, picture_HSV, resultHSV, maskHSV);
 
-  cv::imshow("Result HSV filter", resultHSV);
+  imshow("Result HSV filter", resultHSV);
 
-  return resultHSV;
+  Mat img;
+  Mat element_alg = getStructuringElement(MORPH_ELLIPSE, Size(5, 5));
+  morphologyEx(resultHSV, img, MORPH_OPEN, element_alg);
+  morphologyEx(img, img, MORPH_CLOSE, element_alg);
+
+  cvtColor(img,img,COLOR_HSV2BGR);
+  cvtColor(img,img,COLOR_BGR2GRAY);
+
+  threshold(img, img, 0, 255, THRESH_BINARY + THRESH_OTSU);
+
+  medianBlur(img, img, 9);
+
+  imshow("Tresholded", img);
+
+  return img;
+
 }
 
 int main(int argc, char *argv[]) {
   if (argc != 2) return EXIT_FAILURE;
 
-  cv::Mat img_rgb = cv::imread(argv[1], CV_LOAD_IMAGE_COLOR);
+  Mat img_rgb = imread(argv[1], CV_LOAD_IMAGE_COLOR);
 
-  cv::medianBlur(img_rgb, img_rgb, 9);
+  medianBlur(img_rgb, img_rgb, 5);
 
   Mat img = filter(img_rgb);
 
-  cv::imshow("algo", img);
+  Mat skel(img.size(), CV_8UC1, Scalar(0));
+  Mat temp, eroded;
 
-  cv::Mat element_alg = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5));
-  cv::morphologyEx(img, img, cv::MORPH_OPEN, element_alg);
-  cv::morphologyEx(img, img, cv::MORPH_CLOSE, element_alg);
-
-  cv::imshow("algo clos", img);
-
-  cvtColor(img,img,COLOR_HSV2BGR);
-  cvtColor(img,img,COLOR_BGR2GRAY);
-
-  cv::threshold(img, img, 0, 255, cv::THRESH_BINARY + cv::THRESH_OTSU);
-
-  cv::medianBlur(img, img, 9);
-
-  cv::imshow("tresh", img);
-
-  cv::Mat skel(img.size(), CV_8UC1, cv::Scalar(0));
-  cv::Mat temp, eroded;
-
-  cv::Mat element = cv::getStructuringElement(cv::MORPH_CROSS, cv::Size(3, 3));
+  Mat element = getStructuringElement(MORPH_CROSS, Size(3, 3));
 
   do {
-    cv::erode(img, eroded, element);
-    cv::dilate(eroded, temp, element); // temp = open(img)
-    cv::subtract(img, temp, temp);
-    cv::bitwise_or(skel, temp, skel);
+    erode(img, eroded, element);
+    dilate(eroded, temp, element); // temp = open(img)
+    subtract(img, temp, temp);
+    bitwise_or(skel, temp, skel);
     eroded.copyTo(img);
-  } while (cv::countNonZero(img) != 0);
+  } while (countNonZero(img) != 0);
 
-  cv::imshow("final", skel);
+  imshow("Skeleton", skel);
 
-  cv::vector<cv::Vec4i> lines;
+  vector<Vec4i> lines;
 
-  cv::HoughLinesP(skel, lines, 10.0, 2 * CV_PI/180.0, 80, 10, 30);
+  HoughLinesP(skel, lines, 1.0, 1 * CV_PI/180.0, 25, 50, 60);
 
   std::cout << "Found : " << lines.size() << std::endl;
 
-  cv::vector<cv::Point2i> intersections;
+  vector<Point2i> intersections;
 
-  cv::vector<Mat> intersections_types;
+  vector<Mat> intersections_types;
 
   for(size_t i = 0; i < lines.size(); i++) {
-      cv::Point2i oi = cv::Point2i(lines[i][0], lines[i][1]);
-      cv::Point2i pi = cv::Point2i(lines[i][2], lines[i][3]);
+      Point2i oi = Point2i(lines[i][0], lines[i][1]);
+      Point2i pi = Point2i(lines[i][2], lines[i][3]);
 
-      line(img_rgb, oi, pi, cv::Scalar(0,0,255), 1, 8);
+      line(img_rgb, oi, pi, Scalar(0,0,255), 1, 8);
 
       for(size_t j = 0; j < lines.size(); j++) {
 
-          cv::Point2i oj = cv::Point2i(lines[j][0], lines[j][1]);
-          cv::Point2i pj = cv::Point2i(lines[j][2], lines[j][3]);
+          Point2i oj = Point2i(lines[j][0], lines[j][1]);
+          Point2i pj = Point2i(lines[j][2], lines[j][3]);
 
           if (lines[i] != lines[j]) {
-              cv::Point2i intersect = GetIntersection(oi, pi, oj, pj);
+              Point2i intersect = GetIntersection(oi, pi, oj, pj);
               int c = GetIntersectType(oi, pi, oj, pj, intersect);
 
-              if (intersect != cv::Point2i()) {
+              if (intersect != Point2i()) {
                   bool already = false;
                   for (size_t k = 0; k < intersections.size(); k++) {
                       if (norm(intersections[k] - intersect) < 50.0) {
@@ -170,7 +168,7 @@ int main(int argc, char *argv[]) {
                   }
 
                   if (!already && c < 3) {
-                          Mat votes = Mat(1, 3, CV_8U, cv::Scalar(0));
+                          Mat votes = Mat(1, 3, CV_8U, Scalar(0));
                           intersections.push_back(intersect);
                           votes.at<uchar>(c)++;
                           intersections_types.push_back(votes);
@@ -185,26 +183,26 @@ int main(int argc, char *argv[]) {
 
     std::cout << v << std::endl;
 
-    cv::Point min_loc;
-    cv::Point max_loc;
-    cv::minMaxLoc(v, NULL, NULL, NULL, &max_loc);
+    Point min_loc;
+    Point max_loc;
+    minMaxLoc(v, NULL, NULL, NULL, &max_loc);
 
     std::cout << min_loc << max_loc << std::endl;
 
     int c = max_loc.x;
-    cv::Point2i intersect = intersections[k];
+    Point2i intersect = intersections[k];
     if (c == 0) {
-        circle(img_rgb, intersect, 10, cv::Scalar(0, 255, 0));
+        circle(img_rgb, intersect, 20, Scalar(0, 255, 0), 3);
     } else if (c == 1) {
-        circle(img_rgb, intersect, 10, cv::Scalar(255, 0, 0));
+        circle(img_rgb, intersect, 20, Scalar(255, 0, 0), 3);
     } else if (c == 2) {
-        circle(img_rgb, intersect, 10, cv::Scalar(255, 0, 255));
+        circle(img_rgb, intersect, 20, Scalar(255, 0, 255), 3);
     } else {
-        circle(img_rgb, intersect, 10, cv::Scalar(0, 0, 255));
+        circle(img_rgb, intersect, 20, Scalar(0, 0, 255), 3);
     }
   }
-  cv::imshow("final2", img_rgb);
+  imshow("Final", img_rgb);
 
-  cv::waitKey(0);
+  waitKey(0);
   return 0;
 }
